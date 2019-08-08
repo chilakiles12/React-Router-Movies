d1803a2eec57158f79315be4b5ecbf9246571a1e
eric <Eric355402@yahoo.com>
2019-08-07 21:35:18 -0600

    routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div className="movie-card">
+        <BrowserRouter>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/Movie/movies/:id" />
+        </BrowserRouter>
+      </div>
     </div>
   );
 };
