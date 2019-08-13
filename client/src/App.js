d152dbbcd6349d37fe4500815addb007f67ee693
eric <Eric355402@yahoo.com>
2019-08-12 23:34:50 -0600

    added render ...props to app.js routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
-import Home from './Movies/Home';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -16,11 +16,14 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+
       <div className="movie-card">
         <BrowserRouter>
-          <Route exact path="/" component={Home} />
-          <Route path="/MovieList" component={MovieList} />
-          <Route path="/movie/:id" component={Movie} />
+          <Link to={'/'} className="home-button">
+            Home
+          </Link>
+          <Route exact path="/" render={props => <MovieList {...props} />} />
+          <Route path="/movie/:id" render={props => <Movie {...props} />} />
         </BrowserRouter>
       </div>
     </div>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import MovieList from './MovieList';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
     <nav>
       <ul>
-        <li className="home-button">Home</li>
         <li className="home-button">Movie List</li>
       </ul>
     </nav>
